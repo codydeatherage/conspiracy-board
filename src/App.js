5f616c79dbeea9809352da8bc4a11e58f9b5171f
codydeatherage <cdeatherage14@gmail.com>
2021-03-27 21:53:39 -0700

    resizable component working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { Resizable, ResizableBox } from 'react-resizable'
+import { useState } from 'react'
 
-function App() {
+const App = (props) => {
+  const [state, setState] = useState({
+    width: 200,
+    height: 200,
+    absoluteWidth: 200,
+    absoluteHeight: 200,
+    absoluteLeft: 0,
+    absoluteTop: 0,
+  });
+
+  const onResetClick = () => {
+    setState({ width: 200, height: 200, absoluteWidth: 200, absoluteHeight: 200 });
+  };
+
+  // On top layout
+  const onResize = (event, { element, size, handle }) => {
+    setState({ width: size.width, height: size.height });
+  };
+
+  const onResizeAbsolute = (event, { element, size, handle }) => {
+    setState((state) => {
+      let newLeft = state.absoluteLeft;
+      let newTop = state.absoluteTop;
+      const deltaHeight = size.height - state.absoluteHeight;
+      const deltaWidth = size.width - state.absoluteWidth;
+      if (handle[0] === 'n') {
+        newTop -= deltaHeight;
+      } else if (handle[0] === 's') {
+        newTop += deltaHeight;
+      }
+      if (handle[handle.length - 1] === 'w') {
+        newLeft -= deltaWidth;
+      } else if (handle[handle.length - 1] === 'e') {
+        newLeft += deltaWidth;
+      }
+
+      return {
+        absoluteWidth: size.width,
+        absoluteHeight: size.height,
+        absoluteLeft: newLeft,
+        absoluteTop: newTop,
+      }
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="layoutRoot">
+      <ResizableBox
+        className="custom-box box"
+        width={200}
+        height={200}
+        handle={<span className="custom-handle custom-handle-se" />}
+        handleSize={[8, 8]}>
+        <span className="text">{"<ResizableBox> with custom overflow style & handle in SE corner."}</span>
+      </ResizableBox>
     </div>
   );
 }

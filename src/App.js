1a173737570137e95ae8611aa68cfd2c2c007b80
codydeatherage <cdeatherage14@gmail.com>
2021-03-29 21:46:56 -0700

    resizable practice

--- src/App.js
+++ src/App.js
@@ -1,62 +1,47 @@
 import './App.css';
-import { Resizable, ResizableBox } from 'react-resizable'
+import { Rnd } from 'react-rnd'
 import { useState } from 'react'
 
 const App = (props) => {
   const [state, setState] = useState({
     width: 200,
     height: 200,
-    absoluteWidth: 200,
-    absoluteHeight: 200,
-    absoluteLeft: 0,
-    absoluteTop: 0,
+    x: 0,
+    y: 0
   });
 
-  const onResetClick = () => {
-    setState({ width: 200, height: 200, absoluteWidth: 200, absoluteHeight: 200 });
-  };
 
-  // On top layout
-  const onResize = (event, { element, size, handle }) => {
-    setState({ width: size.width, height: size.height });
-  };
-
-  const onResizeAbsolute = (event, { element, size, handle }) => {
-    setState((state) => {
-      let newLeft = state.absoluteLeft;
-      let newTop = state.absoluteTop;
-      const deltaHeight = size.height - state.absoluteHeight;
-      const deltaWidth = size.width - state.absoluteWidth;
-      if (handle[0] === 'n') {
-        newTop -= deltaHeight;
-      } else if (handle[0] === 's') {
-        newTop += deltaHeight;
-      }
-      if (handle[handle.length - 1] === 'w') {
-        newLeft -= deltaWidth;
-      } else if (handle[handle.length - 1] === 'e') {
-        newLeft += deltaWidth;
-      }
-
-      return {
-        absoluteWidth: size.width,
-        absoluteHeight: size.height,
-        absoluteLeft: newLeft,
-        absoluteTop: newTop,
-      }
-    })
-  }
-  
   return (
     <div className="layoutRoot">
-      <ResizableBox
-        className="custom-box box"
-        width={200}
-        height={200}
-        handle={<span className="custom-handle custom-handle-se" />}
-        handleSize={[8, 8]}>
-        <span className="text">{"<ResizableBox> with custom overflow style & handle in SE corner."}</span>
-      </ResizableBox>
+      <div className="src">
+        Permanent
+      </div>
+      <Rnd
+        className="bg-dark"
+        default={{
+          x: 0,
+          y: 0,
+          width: 320,
+          height: 200,
+        }}
+      >
+        Rnd
+      </Rnd>
+      <Rnd
+      className="bg-dark"
+        size={{ width: state.width, height: state.height }}
+        position={{ x: state.x, y: state.y }}
+        onDragStop={(e, d) => {setState({ x: d.x, y: d.y }) }}
+        onResizeStop={(e, direction, ref, delta, position) => {
+          setState({
+            width: ref.style.width,
+            height: ref.style.height,
+            ...position,
+          });
+        }}
+      >
+        001
+</Rnd>
     </div>
   );
 }

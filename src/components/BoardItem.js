116f33da8f00cf074004c456e350960f9ae9e5f5
codydeatherage <cdeatherage14@gmail.com>
2021-04-03 23:12:31 -0700

    adding upload

--- src/components/BoardItem.js
+++ src/components/BoardItem.js
@@ -1,15 +1,37 @@
 import { Rnd } from 'react-rnd'
+import axios from 'axios'
 import { useState } from 'react'
 
 const BoardItem = (props) => {
   /* const [children, changeChildren] = useState(0); */
+  const [selectedFile, changeFile] = useState(null);
   const [state, setState] = useState({
     width: 200,
     height: 200,
     x: 0,
     y: 0
   });
+  const onFileChange = e => {
+    changeFile(e.target.files[0]);
+  }
 
+  const onFileUpload = () =>{
+    const formData = new FormData();
+    if(selectedFile.name){
+      formData.append("myFile", selectedFile, selectedFile.name);
+    }
+    axios.post("api/uploadfile", formData);
+  }
+
+  const fileData = () =>{
+    if(selectedFile){
+      return(
+        <div>
+          <h1>sFUILfewwwwwwwwwwwfweEASDF</h1>
+        </div>
+      )
+      } 
+  }
 
 return (
   <div className="layoutRoot">
@@ -17,8 +39,8 @@ return (
       className="bg-dark"
       size={{ width: state.width, height: state.height }}
       position={{ x: state.x, y: state.y }}
-      onDragStop={(d) => { setState({ x: d.x, y: d.y }) }}
-      onResizeStop={(ref, position) => {
+      onDragStop={(e, d) => { setState({ x: d.x, y: d.y }) }}
+      onResizeStop={(e, direction, ref, delta, position) => {
         setState({
           width: ref.style.width,
           height: ref.style.height,
@@ -26,8 +48,11 @@ return (
         });
       }}
     >
-      BOXBOXBOXOBOXBOX
-      
+      001
+      <input type="file" onChange={onFileChange} />
+      <button onClick={onFileUpload}>
+                  Upload!
+                </button>
       </Rnd>
   </div>
 );
